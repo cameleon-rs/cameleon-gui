fa8ac1a0cfb0fd7083d8e9bb101d5c326a49baf7
Yawara ISHIDA <ni732h.sphl@gmail.com>
2021-06-01 13:47:40 +0900

    Replace toggle button with open and close buttons

--- src/control.rs
+++ src/control.rs
@@ -13,46 +13,42 @@ pub enum Msg {
 
 #[derive(Debug)]
 pub enum OutMsg {
-    Open(CameraId),
-    Close(CameraId),
-    StartStreaming(CameraId, PayloadReceiver),
-    StopStreaming(CameraId),
+    Opened(CameraId),
+    Closed(CameraId),
+    StreamingStarted(CameraId, PayloadReceiver),
+    StreamingStopped(CameraId),
     None,
 }
 
 #[derive(Debug, Default)]
 pub struct Control {
-    toggle: button::State,
+    open: button::State,
+    close: button::State,
     start: button::State,
     stop: button::State,
 }
 
 impl Control {
     pub fn view(&mut self, ctx: &Context) -> Element<Msg> {
-        let selected = ctx.selected();
-        let toggle = if let Some(selected) = selected {
-            if selected.is_opened(ctx) {
-                Button::new(&mut self.toggle, Text::new("Close")).on_press(Msg::Close)
-            } else {
-                Button::new(&mut self.toggle, Text::new("Open")).on_press(Msg::Open)
-            }
-        } else {
-            Button::new(&mut self.toggle, Text::new("Open"))
-        };
+        let mut open = Button::new(&mut self.open, Text::new("Open"));
+        let mut close = Button::new(&mut self.close, Text::new("Close"));
         let mut start = Button::new(&mut self.start, Text::new("Start"));
         let mut stop = Button::new(&mut self.stop, Text::new("Stop"));
 
-        if let Some(selected) = selected {
+        if let Some(selected) = ctx.selected() {
             if selected.is_opened(ctx) {
+                close = close.on_press(Msg::Close);
                 if selected.is_streaming(ctx) {
-                    stop = stop.on_press(Msg::StopStreaming);
+                    stop = stop.on_press(Msg::StopStreaming)
                 } else {
                     start = start.on_press(Msg::StartStreaming)
                 }
+            } else {
+                open = open.on_press(Msg::Open)
             }
         }
 
-        let content = Row::new().push(toggle).push(start).push(stop);
+        let content = Row::new().push(open).push(close).push(start).push(stop);
         Container::new(content)
             .width(Length::Fill)
             .style(WithBorder)
@@ -61,38 +57,22 @@ impl Control {
 
     pub fn update(&mut self, msg: Msg, ctx: &mut Context) -> Result<OutMsg> {
         match msg {
-            Msg::Open => {
-                if let Some(cam) = ctx.selected() {
-                    cam.open(ctx)?;
-                    Ok(OutMsg::Open(cam))
-                } else {
-                    Ok(OutMsg::None)
-                }
-            }
-            Msg::Close => {
-                if let Some(cam) = ctx.selected() {
-                    cam.close(ctx)?;
-                    Ok(OutMsg::Close(cam))
-                } else {
-                    Ok(OutMsg::None)
-                }
-            }
-            Msg::StartStreaming => {
-                if let Some(cam) = ctx.selected() {
-                    let recevier = cam.start_streaming(ctx)?;
-                    Ok(OutMsg::StartStreaming(cam, recevier))
-                } else {
-                    Ok(OutMsg::None)
-                }
-            }
-            Msg::StopStreaming => {
-                if let Some(cam) = ctx.selected() {
-                    cam.stop_streaming(ctx)?;
-                    Ok(OutMsg::StopStreaming(cam))
-                } else {
-                    Ok(OutMsg::None)
-                }
-            }
+            Msg::Open => ctx.selected().map_or(Ok(OutMsg::None), |cam| {
+                cam.open(ctx)?;
+                Ok(OutMsg::Opened(cam))
+            }),
+            Msg::Close => ctx.selected().map_or(Ok(OutMsg::None), |cam| {
+                cam.close(ctx)?;
+                Ok(OutMsg::Closed(cam))
+            }),
+            Msg::StartStreaming => ctx.selected().map_or(Ok(OutMsg::None), |cam| {
+                let receiver = cam.start_streaming(ctx)?;
+                Ok(OutMsg::StreamingStarted(cam, receiver))
+            }),
+            Msg::StopStreaming => ctx.selected().map_or(Ok(OutMsg::None), |cam| {
+                cam.stop_streaming(ctx)?;
+                Ok(OutMsg::StreamingStopped(cam))
+            }),
         }
     }
 }
